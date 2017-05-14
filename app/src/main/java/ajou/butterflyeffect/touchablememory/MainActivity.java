264bf5bc9b1b1171119b6edcda27292db5a8c839
ParkSungmi <sungmi1995@gmail.com>
2017-05-14 21:54:36 +0900

    add temp file to merge

--- app/src/main/java/ajou/butterflyeffect/touchablememory/MainActivity.java
+++ app/src/main/java/ajou/butterflyeffect/touchablememory/MainActivity.java
@@ -1,13 +1,34 @@
 package ajou.butterflyeffect.touchablememory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_select = (Button) findViewById(R.id.btn_select);
+    Button btn_regist = (Button) findViewById(R.id.btn_regist);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_select.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Gallery.class);
+                startActivity(intent);
+
+
+            }
+        });
+
     }
+
+
+
+
 }
