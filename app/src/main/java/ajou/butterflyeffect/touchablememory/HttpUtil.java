dcd3693e88b3ee04ea1789a65e117fdc2eb0c86d
ParkSungmi <sungmi1995@gmail.com>
2017-05-17 17:12:56 +0900

    correct HttpUtil file

--- app/src/main/java/ajou/butterflyeffect/touchablememory/HttpUtil.java
+++ app/src/main/java/ajou/butterflyeffect/touchablememory/HttpUtil.java
@@ -1,8 +1,17 @@
 package ajou.butterflyeffect.touchablememory;
 
+import android.content.res.Resources;
 import android.os.AsyncTask;
+import android.os.Environment;
+import android.util.Log;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -13,39 +22,140 @@ import java.net.URL;
  * Created by tpdlq on 2017-05-16.
  */
 
-public class HttpUtil extends AsyncTask<String, Void, Void> {
+public class HttpUtil extends AsyncTask<Resources, Void, Void> {
+
     @Override
-    public Void doInBackground(String... params) {
+    public Void doInBackground(Resources... params) {
+
+        //InputStream is = params[0].openRawResource(R.raw.rose);
+
+
+        String attachmentName = "rose";
+        String attachmentFileName = "rose.jpg";
+        String crlf = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+
         try {
-            String url = "http://example.com/test.jsp";
-            URL obj = new URL(url);
-            HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+            HttpURLConnection httpURLConnection = null;
+            URL url = new URL("http://52.79.156.99:8080/upload");
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setUseCaches(false);
+            httpURLConnection.setDoOutput(true);
 
-            conn.setReadTimeout(10000);
-            conn.setConnectTimeout(15000);
-            conn.setRequestMethod("POST");
-            conn.setDoInput(true);
-            conn.setDoOutput(true);
-            conn.setRequestProperty("Content-Type","application/json");
 
-            byte[] outputInBytes = params[0].getBytes("UTF-8");
-            OutputStream os = conn.getOutputStream();
-            os.write( outputInBytes );
-            os.close();
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setRequestProperty("Connection", "Keep-Alive");
+            httpURLConnection.setRequestProperty("Cache-Control", "no-cache");
+            httpURLConnection.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+            //httpURLConnection.setRequestProperty("id", "image");
 
-            int retCode = conn.getResponseCode();
+            DataOutputStream request = new DataOutputStream(httpURLConnection.getOutputStream());
 
-            InputStream is = conn.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            String line;
-            StringBuffer response = new StringBuffer();
-            while((line = br.readLine()) != null) {
-                response.append(line);
-                response.append('\r');
+
+            // String param = URLEncoder.encode("aaaaaaa","UTF-8");
+            //request.writeBytes("aaaaa");
+
+
+
+
+            //request.writeBytes(twoHyphens + boundary + crlf);
+            //request.writeBytes("Content-Disposition: form-data; test=\"" + attachmentName + "\";filename=\"" + attachmentFileName + "\"" + crlf);
+            //request.writeBytes(crlf);
+
+
+
+
+
+            String cameraTempFilePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/DCIM/Camera/5.jpg";
+
+            File file = new File(cameraTempFilePath);
+
+
+
+            int size = (int) file.length();
+
+
+            byte[] bytes = new byte[size];
+            try {
+                BufferedInputStream buf = new BufferedInputStream(new FileInputStream(file));
+                buf.read(bytes, 0, bytes.length);
+                buf.close();
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
-            br.close();
+            Log.d("dbgaa>>",bytes.length+"");
 
-            String res = response.toString();
+
+            request.write(bytes, 0, bytes.length);
+            request.writeBytes("ok");
+
+
+                /*
+
+                 ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+                int nRead;
+                byte[] data = new byte[16384];
+
+                while ((nRead = is.read(data, 0, data.length)) != -1) {
+                    buffer.write(data, 0, nRead);
+                }
+
+                buffer.flush();
+*/
+            //data = buffer.toByteArray();
+/*
+                    byte[] pixels = new byte[bitmap.getWidth() * bitmap.getHeight()];
+                    for (int i = 0; i < bitmap.getWidth(); ++i) {
+                        for (int j = 0; j < bitmap.getHeight(); ++j) {
+                            //we're interested only in the MSB of the first byte,
+                            //since the other 3 bytes are identical for B&W images
+                            pixels[i + j] = (byte) ((bitmap.getPixel(i, j) & 0x80) >> 7);
+                        }
+                    }
+*/
+            //request.write(pixels);
+
+            //request.write(buffer.toByteArray());
+            //request.write(bytes);
+            //request.writeBytes(crlf);
+            //request.writeBytes(twoHyphens + boundary + twoHyphens + crlf);
+
+
+            request.flush();
+            request.close();
+
+            int code = httpURLConnection.getResponseCode();
+
+            Log.d("code : "+code,"");
+
+            if(code == httpURLConnection.HTTP_OK) {
+                InputStream responseStream = new BufferedInputStream(httpURLConnection.getInputStream());
+
+                BufferedReader responseStreamReader = new BufferedReader(new InputStreamReader(responseStream));
+
+                String line = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ((line = responseStreamReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                responseStreamReader.close();
+
+                String response = stringBuilder.toString();
+
+                Log.d("dbg>>", response);
+
+                responseStream.close();
+            }
+
+            httpURLConnection.disconnect();
+
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -54,4 +164,9 @@ public class HttpUtil extends AsyncTask<String, Void, Void> {
         return null;
 
     }
+
+
+
 }
+
+
